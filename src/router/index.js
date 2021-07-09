e3f3d5181eee4938419afd6dea3d71d24b28267b
asad-ali-web-dev <81629007+asad-ali-web-dev@users.noreply.github.com>
2021-07-10 01:49:17 +0500

    removed hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../pages/Home.vue'
 import Donations from '../pages/Donations.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
 
